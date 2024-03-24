ef77a4c5039e2f266c38628d2ce2e0f8cd851339
Chockplay <charly.flores.san@gmail.com>
2024-03-23 22:24:08 -0600

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,21 @@ const Button = (props) => {
     </button>
   )
 }
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+  const average = (props.good + (props.bad * -1))/total
+  const positive = ((props.good)/total)*100
+  return(
+      <>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad} </p>
+      <p>all {total} </p>
+      <p>average {average} </p>
+      <p>positive {positive} %</p>
+      </>
+  )
+}
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -19,9 +34,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text={'Neutral'} />
       <Button handleClick={() => setBad(bad + 1)} text={'Bad'} />
       <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad} </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
